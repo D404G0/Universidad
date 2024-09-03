e2db5efb77b7a02ff8fed1bba7fd8e37226ea72b
D404G0 <diegol-patinof@unilibre.edu.co>
2024-09-03 13:40:12 -0500

    v4

--- Arquitectura de datos/ciclismo.py
+++ Arquitectura de datos/ciclismo.py
@@ -1,3 +1,6 @@
+import random
+
+#Funcion para ingresar valores numericos
 def read_int(message):
     while True:
         try:
@@ -5,6 +8,7 @@ def read_int(message):
         except ValueError:
             print("El dato que ingreso no es numerico, intentelo nuevamente.")
 
+#Funcion para ingresar valores numericos con ecepciones
 def read_num(mensaje):
     while True:
         try:
@@ -15,6 +19,7 @@ def read_num(mensaje):
         except ValueError:
             print("El valor tiene que ser mayor a 0. ")
 
+#Funcion para ingresar palabras
 def read_str(mensaje):
     while True:
         word = input(mensaje)
@@ -22,21 +27,34 @@ def read_str(mensaje):
             print("Tiene que ingresar una palabra.")
         elif word.isalpha():  
             return word
+        
+#Funcion para gener un numero aleatorio de 0 a 60.
+def second_minutes():
+    random.randrange(0, 59)
+
+#Funcion paara generar un nuemro aleatorio desde 80 hasta 100
+def hour():
+    random.randrange(80, 100)
   
 napl = [] #Lista de los nombres de los participantes
 nate = [] #Lista de los nombres del equipo al que pertenece
+time = [] #Lista para guardar el tiempo del jugador
 
 
 #inicio de codigo.
-lap = read_int("Ingrese el número de participantes que tiene su carrera: ")
-for i in range(lap):
+part = read_int("Ingrese el número de participantes que tiene su carrera: ")
+for i in range(part):
     name = read_str("Ingrese el nombre del ciclista: ")
     napl.append(name)
     name_team = read_str(f"Ingrese el nombre del equipo al que pertenece el ciclista {name}. ")
     nate.append(name_team)
+    time_var = (f"Hora{hour()} - Minutos{second_minutes} - Segundos{second_minutes}.")
+    time.append(time_var)
+
 
 print(napl)
 print(nate)
+print(time)
 
 
 
