37232db084d5b0d42d8d4cf6a52d3a7ceb990ea4
D404G0 <diegol-patinof@unilibre.edu.co>
2024-09-04 15:28:56 -0500

    v4

--- Arquitectura de datos/ciclismo.py
+++ Arquitectura de datos/ciclismo.py
@@ -28,35 +28,40 @@ def read_str(mensaje):
         elif word.isalpha():  
             return word
         
-#Funcion para gener un numero aleatorio de 0 a 60.
-def second_minutes():
-    random.randrange(0, 59)
+#Funcion para gener un numero aleatorio de 0 a 59.
+def semi():
+    k = random.randrange(0, 59)
+    return k
 
 #Funcion paara generar un nuemro aleatorio desde 80 hasta 100
 def hour():
-    random.randrange(80, 100)
+    x = random.randrange(80, 100)
+    return x
   
-napl = [] #Lista de los nombres de los participantes
-nate = [] #Lista de los nombres del equipo al que pertenece
-time = [] #Lista para guardar el tiempo del jugador
+#Diccionario donde se almacenara los datos del tour
+tour_france = {"Nombre" : [], "Numero" : [], "Equipo" : [], "Tiempo" : [] }
 
 
 #inicio de codigo.
 part = read_int("Ingrese el número de participantes que tiene su carrera: ")
 for i in range(part):
     name = read_str("Ingrese el nombre del ciclista: ")
-    napl.append(name)
-    name_team = read_str(f"Ingrese el nombre del equipo al que pertenece el ciclista {name}. ")
-    nate.append(name_team)
-    time_var = (f"Hora{hour()} - Minutos{second_minutes} - Segundos{second_minutes}.")
-    time.append(time_var)
+    tour_france["Nombre"].append(name)
+
+    id = read_num(f"Ingrese el numero del ciclista {name}: ")
+    tour_france["Numero"].append(id)
+
+    name_team = read_str(f"Ingrese el nombre del equipo al que pertenece el ciclista {name}: ")
+    tour_france["Equipo"].append(name_team)
+
+    print("------------------------------------------------")
+    time_var = (f"{hour()}h - {semi()}' - {semi()}''.")
+    tour_france["Tiempo"].append(time_var)
+
+for participante in tour_france:
+    for keys, values in participante:
+        print(keys, "-", values)
 
 
-print(napl)
-print(nate)
-print(time)
 
-
-
-
-# v2
+# v4
